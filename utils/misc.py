f178e0d668bcff535fb4dc3350ccd46583bcc51c
rufaelfekadu <rufaelfekadu48@gmail.com>
2024-06-23 17:16:41 +0400

    updated to use dassl

---
+++ utils/misc.py
@@ -0,0 +1,132 @@
+from dassl.utils import seed_utils
+from dassl.config import get_cfg_default
+from yacs.config import CfgNode as CN
+
+
+def reset_cfg(cfg, args):
+    if args.root:
+        cfg.DATASET.ROOT = args.root
+
+    if args.output_dir:
+        cfg.OUTPUT_DIR = args.output_dir
+
+    if args.resume:
+        cfg.RESUME = args.resume
+
+    if args.seed:
+        cfg.SEED = args.seed
+
+    if args.source_domains:
+        cfg.DATASET.SOURCE_DOMAINS = args.source_domains
+
+    if args.target_domains:
+        cfg.DATASET.TARGET_DOMAINS = args.target_domains
+
+    if args.transforms:
+        cfg.INPUT.TRANSFORMS = args.transforms
+
+    if args.trainer:
+        cfg.TRAINER.NAME = args.trainer
+
+    if args.backbone:
+        cfg.MODEL.BACKBONE.NAME = args.backbone
+
+    if args.head:
+        cfg.MODEL.HEAD.NAME = args.head
+
+
+def extend_cfg(cfg, args):
+    """
+    Add new config variables.
+
+    E.g.
+        from yacs.config import CfgNode as CN
+        cfg.TRAINER.MY_MODEL = CN()
+        cfg.TRAINER.MY_MODEL.PARAM_A = 1.
+        cfg.TRAINER.MY_MODEL.PARAM_B = 0.5
+        cfg.TRAINER.MY_MODEL.PARAM_C = False
+    """
+
+    cfg.TRAINER.COOP = CN()
+    cfg.TRAINER.COOP.N_CTX = 16  # number of context vectors
+    cfg.TRAINER.COOP.CSC = False  # class-specific context
+    cfg.TRAINER.COOP.CTX_INIT = ""  # initialization words
+    cfg.TRAINER.COOP.PREC = "fp32"  # fp16, fp32, amp
+    cfg.TRAINER.COOP.CLASS_TOKEN_POSITION = "end"  # 'middle' or 'end' or 'front'
+
+    cfg.TRAINER.COCOOP = CN()
+    cfg.TRAINER.COCOOP.N_CTX = 16  # number of context vectors
+    cfg.TRAINER.COCOOP.CTX_INIT = ""  # initialization words
+    cfg.TRAINER.COCOOP.PREC = "fp16"  # fp16, fp32, amp
+
+    cfg.DATASET.SUBSAMPLE_CLASSES = "all"  # all, base or new
+
+    cfg.txt_cls = args.txt_cls
+    cfg.gpt_prompts = args.gpt_prompts
+    cfg.desc_noise = args.desc_noise
+    cfg.k_desc = args.k_desc
+    cfg.classifer_random_weights = args.classifer_random_weights
+    cfg.classifier_frozen_weights = args.classifier_frozen_weights
+    cfg.ve_unshared = args.ve_unshared
+    cfg.desc_emb = args.desc_emb
+
+
+def setup_cfg(args):
+    cfg = get_cfg_default()
+    extend_cfg(cfg)
+
+    # 1. From the dataset config file
+    if args.dataset_config_file:
+        cfg.merge_from_file(args.dataset_config_file)
+
+    # 2. From the method config file
+    if args.config_file:
+        cfg.merge_from_file(args.config_file)
+
+    # 3. From input arguments
+    reset_cfg(cfg, args)
+
+    # 4. From optional input arguments
+    cfg.merge_from_list(args.opts)
+
+    return cfg
+
+
+
+
+class lossmeter:
+    """Compute and store the average and current value.
+
+    Examples::
+        >>> # 1. Initialize a meter to record loss
+        >>> losses = AverageMeter()
+        >>> # 2. Update meter after every mini-batch update
+        >>> losses.update(loss_value, batch_size)
+    """
+
+    def __init__(self, ema=False):
+        """
+        Args:
+            ema (bool, optional): apply exponential moving average.
+        """
+        self.ema = ema
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        if isinstance(val, torch.Tensor):
+            val = val.item()
+
+        self.val = val
+        self.sum += val * n
+        self.count += n
+
+        if self.ema:
+            self.avg = self.avg * 0.9 + self.val * 0.1
+        else:
+            self.avg = self.sum / self.count
